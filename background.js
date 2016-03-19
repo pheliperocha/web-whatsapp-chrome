c6a28fa4fbc5a26df73e70fb509dd25efff4041e
Phelipe Rocha <phelipeafonso@gmail.com>
2016-03-19 18:20:35 -0300

    Added permission take picture, record audio and download files.

--- background.js
+++ background.js
@@ -3,7 +3,8 @@
  *
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
+
+chrome.app.runtime.onLaunched.addListener(function () {
      chrome.app.window.create('index.html', {
           id: 'main',
           bounds: { width: 1024, height: 768 }
--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+var webview = document.querySelector("webview");
+
 function updateWebviews() {
-     var webview = document.querySelector("webview");
      webview.style.height = document.documentElement.clientHeight + "px";
      webview.style.width = document.documentElement.clientWidth + "px";
-};
+}
 
 onload = updateWebviews;
 window.onresize = updateWebviews;
+
+webview.addEventListener('permissionrequest', function (e) {
+     if (e.permission === 'media') {
+          e.request.allow();
+     }
+
+     if (e.permission === 'download') {
+          e.request.allow();
+     }
+});
